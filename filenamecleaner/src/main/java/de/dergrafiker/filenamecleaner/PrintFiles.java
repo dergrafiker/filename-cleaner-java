06bac5d3c20eb3025156ea484f8929a0a79f82ee
dergrafiker <stephan.herder@gmail.com>
2020-05-13 15:52:10 +0200

    find out which chars are remove on cleaning

--- filenamecleaner/src/main/java/de/dergrafiker/filenamecleaner/PrintFiles.java
+++ filenamecleaner/src/main/java/de/dergrafiker/filenamecleaner/PrintFiles.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
@@ -21,12 +26,35 @@ public class PrintFiles extends SimpleFileVisitor<Path> {
         String oldName = file.getName();
         if (FilenameChecker.isInvalid(oldName, file.isDirectory())) {
             String cleaned = FilenameCleaner.clean(oldName, file.isDirectory());
-            LOGGER.info("'{}' => '{}' [{}]",
-                        StringUtils.rightPad(oldName, 70, ' '),
-                        StringUtils.rightPad(cleaned, 70, ' '),
-                        file.getParentFile().getAbsolutePath());
+
+            Set<Character> removedChars = getUniqueLowerCaseChars(oldName);
+            removedChars.removeAll(getUniqueLowerCaseChars(cleaned));
+
+            Set<Character> ignored = new HashSet<>();
+            Collections.addAll(ignored, '[', ']', '(', ')', ',', ';', ' ', '.', '&', '+', '#', '$', '!', '\'');
+
+            removedChars.removeAll(ignored);
+
+            if (removedChars.size() > 0) {
+                LOGGER.info("{} has removed >> {} << {}",
+                            oldName,
+                            StringUtils.join(removedChars),
+                            cleaned);
+            }
+
+//            LOGGER.info("'{}' => '{}' [{}]",
+//                        StringUtils.rightPad(oldName, 70, ' '),
+//                        StringUtils.rightPad(cleaned, 70, ' '),
+//                        file.getParentFile().getAbsolutePath());
         }
 //        LOGGER.info("walked dir {}", dir);
         return CONTINUE;
+    }
+
+    private Set<Character> getUniqueLowerCaseChars(String oldName) {
+        Stream<Character> characterStream = oldName.chars()
+                .mapToObj(c -> (char) Character.toLowerCase(c));
+
+        return characterStream.collect(Collectors.toSet());
     }
 }
