b8b0231a4476852bff0ec7840297163049a0cdda
dergrafiker <stephan.herder@gmail.com>
2020-06-29 23:44:52 +0200

    now caching result for each root

--- filenamecleaner/src/main/java/de/dergrafiker/filenamecleaner/CaseSensivityChecker.java
+++ filenamecleaner/src/main/java/de/dergrafiker/filenamecleaner/CaseSensivityChecker.java
@@ -2,18 +2,29 @@ package de.dergrafiker.filenamecleaner;
 
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CaseSensivityChecker {
 
+    private final Map<Path, Boolean> rootToCaseSensivityMap = new HashMap<>();
+
     public boolean isCaseSensitive(Path path) {
-        String originalFilename = path.getFileName().toString();
+        Path root = path.getRoot();
 
-        Path lc = path.getParent().resolve(originalFilename.toLowerCase());
-        Path uc = path.getParent().resolve(originalFilename.toUpperCase());
+        if (root == null) {
+            throw new IllegalArgumentException(String.format("Path %s must have a root", path));
+        }
 
+        return rootToCaseSensivityMap.computeIfAbsent(root, this::icCi);
+    }
+
+    private boolean icCi(Path root) {
+        String originalFilename = "test";
+        Path lc = root.resolve(originalFilename.toLowerCase());
+        Path uc = root.resolve(originalFilename.toUpperCase());
         return !lc.equals(uc);
     }
 }
--- filenamecleaner/src/test/java/de/dergrafiker/filenamecleaner/CaseSensivityCheckerTest.java
+++ filenamecleaner/src/test/java/de/dergrafiker/filenamecleaner/CaseSensivityCheckerTest.java
@@ -15,7 +15,7 @@ class CaseSensivityCheckerTest {
     private final CaseSensivityChecker caseSensivityChecker = new CaseSensivityChecker();
 
     @Test
-    void name() throws IOException {
+    void name1() throws IOException {
         assumeFalse(SystemUtils.IS_OS_WINDOWS);
         Path tempFile = Files.createTempFile("foo", "bar");
         assertThat(caseSensivityChecker.isCaseSensitive(tempFile)).isTrue();
