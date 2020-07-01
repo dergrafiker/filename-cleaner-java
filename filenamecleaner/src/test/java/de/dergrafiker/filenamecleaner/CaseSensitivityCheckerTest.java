a2e75ae22095f0b7047093ddd8e3fe9d2c929e71
dergrafiker <stephan.herder@gmail.com>
2020-07-01 14:28:18 +0200

    added inverse test

--- filenamecleaner/src/test/java/de/dergrafiker/filenamecleaner/CaseSensitivityCheckerTest.java
+++ filenamecleaner/src/test/java/de/dergrafiker/filenamecleaner/CaseSensitivityCheckerTest.java
@@ -9,15 +9,23 @@ import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class CaseSensitivityCheckerTest {
 
     private final CaseSensitivityChecker caseSensitivityChecker = new CaseSensitivityChecker();
 
     @Test
-    void name1() throws IOException {
+    void whenNotOnWindowsThenFilenamesAreCaseSensitive() throws IOException {
         assumeFalse(SystemUtils.IS_OS_WINDOWS);
         Path tempFile = Files.createTempFile("foo", "bar");
         assertThat(caseSensitivityChecker.isCaseSensitive(tempFile)).isTrue();
+    }
+
+    @Test
+    void whenOnWindowsThenFilenamsAreNotCaseSensitive() throws IOException {
+        assumeTrue(SystemUtils.IS_OS_WINDOWS);
+        Path tempFile = Files.createTempFile("foo", "bar");
+        assertThat(caseSensitivityChecker.isCaseSensitive(tempFile)).isFalse();
     }
 }
