cacf0cf629f450e8246cb59a9e98ffa36f53a673
dergrafiker <stephan.herder@gmail.com>
2021-05-02 12:50:11 +0200

    static imports

--- filenamecleaner/src/main/java/de/dergrafiker/filenamecleaner/FilenameCleaner.java
+++ filenamecleaner/src/main/java/de/dergrafiker/filenamecleaner/FilenameCleaner.java
@@ -2,12 +2,21 @@ package de.dergrafiker.filenamecleaner;
 
 import com.google.common.base.CaseFormat;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.apache.commons.text.WordUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Matcher;
+
+import static de.dergrafiker.filenamecleaner.MatcherUtil.MANY_UPPERCASE_PATTERN;
+import static de.dergrafiker.filenamecleaner.MatcherUtil.getMatcher;
+import static org.apache.commons.lang3.StringUtils.countMatches;
+import static org.apache.commons.lang3.StringUtils.removeEnd;
+import static org.apache.commons.lang3.StringUtils.removeStart;
+import static org.apache.commons.lang3.StringUtils.replace;
+import static org.apache.commons.lang3.StringUtils.replaceChars;
+import static org.apache.commons.lang3.StringUtils.replaceEach;
+import static org.apache.commons.lang3.StringUtils.replaceOnce;
 
 @Component
 public class FilenameCleaner {
@@ -28,98 +37,98 @@ public class FilenameCleaner {
         String output = name.trim();
         output = StringEscapeUtils.unescapeHtml4(output);
 
-        output = StringUtils.removeStart(output, ".");
-        output = StringUtils.removeEnd(output, ".");
-        output = StringUtils.removeStart(output, "-");
+        output = removeStart(output, ".");
+        output = removeEnd(output, ".");
+        output = removeStart(output, "-");
 
         output = replaceUppercaseWords(output);
         output = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, output);
 
         if (isDirectory) {
-            output = StringUtils.replaceChars(output, '.', ' ');
+            output = replaceChars(output, '.', ' ');
         } else {
-            if (StringUtils.countMatches(output, '.') > 1) {
+            if (countMatches(output, '.') > 1) {
                 String baseName = FilenameUtils.getBaseName(output);
-                baseName = StringUtils.replaceChars(baseName, '.', ' ').trim();
+                baseName = replaceChars(baseName, '.', ' ').trim();
 
                 String extension = FilenameUtils.getExtension(output);
                 output = baseName + '.' + extension;
             }
         }
 
-        output = StringUtils.replaceChars(output, "'", " ");
-        output = StringUtils.replaceChars(output, ",", " ");
-        output = StringUtils.replaceChars(output, "`", " ");
-        output = StringUtils.replaceChars(output, "[", " ");
-        output = StringUtils.replaceChars(output, "]", " ");
-        output = StringUtils.replaceChars(output, "(", " ");
-        output = StringUtils.replaceChars(output, ")", " ");
-        output = StringUtils.replaceChars(output, "{", " ");
-        output = StringUtils.replaceChars(output, "}", " ");
-        output = StringUtils.replaceChars(output, "+", " ");
-        output = StringUtils.replaceChars(output, "=", " ");
-        output = StringUtils.replaceChars(output, "!", " ");
-        output = StringUtils.replaceChars(output, "#", " ");
-        output = StringUtils.replaceChars(output, "’", " ");
-        output = StringUtils.replaceChars(output, ";", " ");
-        output = StringUtils.replaceChars(output, "%", " ");
-        output = StringUtils.replaceChars(output, "•", " ");
-        output = StringUtils.replaceChars(output, "♥", " ");
-        output = StringUtils.replaceChars(output, "◕", " ");
-        output = StringUtils.replaceChars(output, "°", " ");
-        output = StringUtils.replaceChars(output, "Ж", " ");
-        output = StringUtils.replaceChars(output, "Ф", " ");
-        output = StringUtils.replaceChars(output, "Д", " ");
-        output = StringUtils.replaceChars(output, "~", " ");
-        output = StringUtils.replaceChars(output, "¡", " ");
-        output = StringUtils.replaceChars(output, "$", " ");
-        output = StringUtils.replaceChars(output, "…", " ");
-        output = StringUtils.replaceChars(output, "^", " ");
-        output = StringUtils.replaceChars(output, "@", " ");
-        output = StringUtils.replaceChars(output, "´", " ");
-        output = StringUtils.replaceChars(output, "„", " ");
-        output = StringUtils.replaceChars(output, "“", " ");
-        output = StringUtils.replaceChars(output, "£", " ");
-        output = StringUtils.replaceChars(output, "∞", " ");
+        output = replaceChars(output, "'", " ");
+        output = replaceChars(output, ",", " ");
+        output = replaceChars(output, "`", " ");
+        output = replaceChars(output, "[", " ");
+        output = replaceChars(output, "]", " ");
+        output = replaceChars(output, "(", " ");
+        output = replaceChars(output, ")", " ");
+        output = replaceChars(output, "{", " ");
+        output = replaceChars(output, "}", " ");
+        output = replaceChars(output, "+", " ");
+        output = replaceChars(output, "=", " ");
+        output = replaceChars(output, "!", " ");
+        output = replaceChars(output, "#", " ");
+        output = replaceChars(output, "’", " ");
+        output = replaceChars(output, ";", " ");
+        output = replaceChars(output, "%", " ");
+        output = replaceChars(output, "•", " ");
+        output = replaceChars(output, "♥", " ");
+        output = replaceChars(output, "◕", " ");
+        output = replaceChars(output, "°", " ");
+        output = replaceChars(output, "Ж", " ");
+        output = replaceChars(output, "Ф", " ");
+        output = replaceChars(output, "Д", " ");
+        output = replaceChars(output, "~", " ");
+        output = replaceChars(output, "¡", " ");
+        output = replaceChars(output, "$", " ");
+        output = replaceChars(output, "…", " ");
+        output = replaceChars(output, "^", " ");
+        output = replaceChars(output, "@", " ");
+        output = replaceChars(output, "´", " ");
+        output = replaceChars(output, "„", " ");
+        output = replaceChars(output, "“", " ");
+        output = replaceChars(output, "£", " ");
+        output = replaceChars(output, "∞", " ");
 
-        output = StringUtils.replace(output, "&", "et");
-        output = StringUtils.replace(output, "µ", "u");
-        output = StringUtils.replace(output, "þ", "th");
-        output = StringUtils.replace(output, "ß", "ss");
+        output = replace(output, "&", "et");
+        output = replace(output, "µ", "u");
+        output = replace(output, "þ", "th");
+        output = replace(output, "ß", "ss");
 
-        output = StringUtils.replace(output, "­", "-");
-        output = StringUtils.replace(output, "–", "-");
-        output = StringUtils.replace(output, "‐", "-");
+        output = replace(output, "­", "-");
+        output = replace(output, "–", "-");
+        output = replace(output, "‐", "-");
 
-        output = StringUtils.replace(output, "ø", "oe");
-        output = StringUtils.replace(output, "œ", "oe");
-        output = StringUtils.replace(output, "æ", "ae");
-        output = StringUtils.replace(output, "Æ", "ae");
-        output = StringUtils.replace(output, "Ø", "O");
-        output = StringUtils.replace(output, "ӱ", "ue");
-        output = StringUtils.replace(output, "ë", "e");
-        output = StringUtils.replace(output, "ĝ", "g");
-        output = StringUtils.replace(output, "å", "a");
-        output = StringUtils.replace(output, "é", "e");
-        output = StringUtils.replace(output, "ï", "i");
-        output = StringUtils.replace(output, "í", "i");
-        output = StringUtils.replace(output, "ñ", "n");
-        output = StringUtils.replace(output, "ç", "c");
-        output = StringUtils.replace(output, "ã", "a");
-        output = StringUtils.replace(output, "Á", "A");
-        output = StringUtils.replace(output, "Â", "A");
-        output = StringUtils.replace(output, "à", "a");
-        output = StringUtils.replace(output, "è", "e");
-        output = StringUtils.replace(output, "ó", "o");
+        output = replace(output, "ø", "oe");
+        output = replace(output, "œ", "oe");
+        output = replace(output, "æ", "ae");
+        output = replace(output, "Æ", "ae");
+        output = replace(output, "Ø", "O");
+        output = replace(output, "ӱ", "ue");
+        output = replace(output, "ë", "e");
+        output = replace(output, "ĝ", "g");
+        output = replace(output, "å", "a");
+        output = replace(output, "é", "e");
+        output = replace(output, "ï", "i");
+        output = replace(output, "í", "i");
+        output = replace(output, "ñ", "n");
+        output = replace(output, "ç", "c");
+        output = replace(output, "ã", "a");
+        output = replace(output, "Á", "A");
+        output = replace(output, "Â", "A");
+        output = replace(output, "à", "a");
+        output = replace(output, "è", "e");
+        output = replace(output, "ó", "o");
 
-        output = StringUtils.replaceEach(output, SEARCH_UMLAUTS, REPLACE_UMLAUTS);
+        output = replaceEach(output, SEARCH_UMLAUTS, REPLACE_UMLAUTS);
 
         output = output.trim();
 
-        output = MatcherUtil.getMatcher("\\s+", output).replaceAll("_");
-        output = MatcherUtil.getMatcher("_+", output).replaceAll("_");
+        output = getMatcher("\\s+", output).replaceAll("_");
+        output = getMatcher("_+", output).replaceAll("_");
 
-        output = StringUtils.replaceEach(output, SEARCH_DASHES, REPLACE_DASHES);
+        output = replaceEach(output, SEARCH_DASHES, REPLACE_DASHES);
 
         return output;
     }
@@ -127,12 +136,12 @@ public class FilenameCleaner {
     String replaceUppercaseWords(final String output) {
         String cleaned = output;
 
-        final Matcher matcher = MatcherUtil.getMatcher(MatcherUtil.MANY_UPPERCASE_PATTERN, output);
+        final Matcher matcher = getMatcher(MANY_UPPERCASE_PATTERN, output);
 
         while (matcher.find()) {
             String ucWord = matcher.group();
             String corrected = WordUtils.capitalizeFully(ucWord);
-            cleaned = StringUtils.replaceOnce(cleaned, ucWord, corrected);
+            cleaned = replaceOnce(cleaned, ucWord, corrected);
         }
         return cleaned;
     }
